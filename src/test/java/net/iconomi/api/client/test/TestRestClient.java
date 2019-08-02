430fdc0d585b6f99003c7a5a14bb0fe2d8373153
Tomaz Cerar <tomaz.cerar@gmail.com>
2019-08-02 10:12:27 +0200

    update dependencies

--- src/test/java/net/iconomi/api/client/test/TestRestClient.java
+++ src/test/java/net/iconomi/api/client/test/TestRestClient.java
@@ -5,6 +5,7 @@ import net.iconomi.api.client.IconomiRestApi;
 import net.iconomi.api.client.model.Balance;
 import net.iconomi.api.client.model.Daa;
 import net.iconomi.api.client.model.DaaChart;
+import net.iconomi.api.client.model.StructureElement;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
@@ -12,21 +13,31 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @Disabled
 public class TestRestClient {
+    private static final String API_KEY = "33e224af6dae6a1317b9722aaeebe4f1a2c07b4c0b541bb5198ab11c3145ca73";
+    private static final String API_SECRET = "254288f3d8e22aed1323081452e9cb761d1c179c786a0983acc399a93a01f795";
+
+    /*private static final String API_KEY = "4a5bbaaa421d2bcbcfe857d33bfc31da66a0242e83c5121da72fa9cfd214d2c5";
+    private static final String API_SECRET = "ee20ba079cd78b66ac08d73f0a73df593b26f26dd0759d6e18dfaad516d508ce";*/
     private final IconomiRestApi api = new IconomiApiBuilder()
             //.setApiKey("api-key", "secret")
+            .setApiKey(API_KEY, API_SECRET)
+            //.setBaseApiUrl("https://api-durkadur.iconomi.com")
             .buildRestApi();
 
     @Test
     public void testApi() throws IOException {
         for (Daa ticker : api.getDaaList()) {
             Daa daa = api.getDaa(ticker.getTicker());
-            System.out.println(String.format("for ticker: '%s' got daa: %s", ticker.getTicker(), daa));
-            assertNotNull(api.getDaaStructure(daa.getTicker()));
+
+            List<StructureElement> structure = api.getDaaStructure(daa.getTicker());
+            assertNotNull(structure);
+            System.out.println(String.format("for ticker: '%s' got: '%s', structure: %s", ticker.getTicker(), daa.getName(), structure));
             try {
                 BigDecimal price = api.getDaaPrice(ticker.getTicker());
                 assertNotNull(price);
@@ -40,10 +51,17 @@ public class TestRestClient {
     }
 
     @Test
-    @Disabled
+    //  @Disabled
     public void testGetUserBalance() throws IOException {
-        Balance b = api.getUserBalance();
-        assertNotNull(b);
+        for (int i = 0; i < 100; i++) {
+            try {
+                Balance b = api.getUserBalance();
+                System.out.println(String.format("%s got balance", i));
+                assertNotNull(b);
+            } catch (IOException e) {
+                System.out.println("could not get balance " + e.getMessage());
+            }
+        }
     }
 
 
